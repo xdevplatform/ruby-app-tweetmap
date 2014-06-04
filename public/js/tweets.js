a0a0bc1bd755cc7949d96330a450ea1513f3de40
noonan <andrew@noonan.cc>
2014-06-04 10:32:21 -0600

    connect to actual host that served the request

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket('ws://localhost:8080/');
+var ws = new WebSocket('ws://' + location.host + ':8080/');
 var map, markers = [];
 var currentLocation;
 var iconBase = '/assets/';
