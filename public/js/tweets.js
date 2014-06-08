541ab7fab42afdf99343b6c3760cd3f7fe4bae86
noonan <andrew@noonan.cc>
2014-06-08 17:14:12 -0600

    play/pause feature

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -4,6 +4,7 @@ var currentLocation;
 var iconBase = '/assets/';
 var iconTweet = iconBase + 'tweet__.png';
 var count = 0;
+var play = true;
 
 // Socket open event
 ws.onopen = function () {
@@ -27,73 +28,75 @@ ws.onclose = function () {
 
 // Handle tweet
 function handleTweet(message) {
-    var tweet = JSON.parse(message.data);
-    var geo = tweet.coordinates;
+    if (play) {
+        var tweet = JSON.parse(message.data);
+        var geo = tweet.coordinates;
 
-    // Check if the geo type is a Point (it can also be a Polygon).
-    if (geo && geo.type === 'Point') {
-        var lat_lon = new google.maps.LatLng(geo.coordinates[1], geo.coordinates[0]);
-        var bounds = map.getBounds();
-        if (bounds && bounds.contains(lat_lon)) {
+        // Check if the geo type is a Point (it can also be a Polygon).
+        if (geo && geo.type === 'Point') {
+            var lat_lon = new google.maps.LatLng(geo.coordinates[1], geo.coordinates[0]);
+            var bounds = map.getBounds();
+            if (bounds && bounds.contains(lat_lon)) {
 
-            // Place marker
-            var marker = new google.maps.Marker({
-                position: lat_lon,
-                map: map,
-                title: tweet["text"],
-                icon: iconTweet,
-                animation: google.maps.Animation.DROP
-            });
+                // Place marker
+                var marker = new google.maps.Marker({
+                    position: lat_lon,
+                    map: map,
+                    title: tweet["text"],
+                    icon: iconTweet,
+                    animation: google.maps.Animation.DROP
+                });
 
-            markers.push(marker);
+                markers.push(marker);
 
-            // Remove old markers when there are more than 300 on the map.
-            if (markers.length > 300) {
-                markers[0].setMap(null);
-                markers.shift();
-            }
+                // Remove old markers when there are more than 300 on the map.
+                if (markers.length > 300) {
+                    markers[0].setMap(null);
+                    markers.shift();
+                }
 
-            // Construct Info Window
-            var profileUrl = "http://www.twitter.com/" + tweet["user"]["screen_name"];
-            var statusUrl = profileUrl + "/status/" + tweet["id_str"];
+                // Construct Info Window
+                var profileUrl = "http://www.twitter.com/" + tweet["user"]["screen_name"];
+                var statusUrl = profileUrl + "/status/" + tweet["id_str"];
 
-            var contentString = '<div id="tweet" class="tweet">' +
-                '<a href="' + profileUrl + '" target="_target">' +
-                '<img class="profile" src="' +
-                tweet["user"]["profile_image_url"] +
-                '">' +
-                '</a>' +
-                '<div class="body">' +
-                '<a href="' + profileUrl + '" target="_target">' +
-                '<div><span class="name">' + tweet["user"]["name"] + '</span>' +
-                ' <span class="handle">@' + tweet["user"]["screen_name"] + '</span></div>' +
-                '</a>' +
-                '<a href="' + statusUrl + '" target="_target" class="text">' +
-                tweet["text"] +
-                '</a>' +
-                '</div>' +
-                '</div>';
+                var contentString = '<div id="tweet" class="tweet">' +
+                    '<a href="' + profileUrl + '" target="_target">' +
+                    '<img class="profile" src="' +
+                    tweet["user"]["profile_image_url"] +
+                    '">' +
+                    '</a>' +
+                    '<div class="body">' +
+                    '<a href="' + profileUrl + '" target="_target">' +
+                    '<div><span class="name">' + tweet["user"]["name"] + '</span>' +
+                    ' <span class="handle">@' + tweet["user"]["screen_name"] + '</span></div>' +
+                    '</a>' +
+                    '<a href="' + statusUrl + '" target="_target" class="text">' +
+                    tweet["text"] +
+                    '</a>' +
+                    '</div>' +
+                    '</div>';
 
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
 
-            google.maps.event.addListener(marker, 'click', function () {
-                $(".tweet").parent().parent().parent().fadeOut();
-                infowindow.open(map, marker);
-            });
+                google.maps.event.addListener(marker, 'click', function () {
+                    $(".tweet").parent().parent().parent().fadeOut();
+                    infowindow.open(map, marker);
+                });
 
-            // Increment counter for TPM
-            count = count + 1;
+                // Increment counter for TPM
+                count = count + 1;
 
-            // Append to list
-            twt.timeline(tweet, {
-                limit: 3
-            }).prependTo("#list")
+                // Append to list
+                twt.timeline(tweet, {
+                    limit: 3
+                }).prependTo("#list")
 
-            // Only keep last 30 tweets in list
-            if ($("#list li").size() > 30) {
-                $('#list li:last-child').remove();
+                // Only keep last 30 tweets in list
+                if ($("#list li").size() > 30) {
+                    $('#list li:last-child').remove();
+                }
             }
         }
     }
@@ -101,7 +104,7 @@ function handleTweet(message) {
 
 // Calculate a TPM every 4s
 setInterval(function () {
-    var tpm = count * 15;
+    var tpm = count * 15 + " TPM";
     $("#tpm").html(tpm);
     count = 0;
 }, 4000);
@@ -144,6 +147,12 @@ function initializeMap() {
         geolocationError();
     }
 }
+
+$("#btn").click(function() {
+    $("#pause").toggle();
+    $("#play").toggle();
+    play = play ? false : true;
+});
 
 // Listen to the load event.
 google.maps.event.addDomListener(window, 'load', initializeMap);
