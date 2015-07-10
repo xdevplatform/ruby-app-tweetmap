7ff38f3f4f5d78f099bcaf9d5cf8539ef8836de5
tworitdash <tworitdash@gmail.com>
2015-07-10 11:50:21 +0530

    tilt/erb added

--- lib/configure_logger.rb
+++ lib/configure_logger.rb
@@ -1,6 +1,6 @@
 require 'logger'
 
-log_file = File.open("./log/app.log", 'w')
+log_file = File.open("app.log", 'w')
 log_file.sync = true
 $logger = Logger.new log_file
 $logger.level = Logger::INFO
